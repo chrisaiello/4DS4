e505f85715e5805b50b623fad38d7565d52c4648
chris <aiellocs@mcmaster.ca>
2016-02-01 11:16:50 -0500

    updating ex3

---
+++ Lab3/exercises/exercise3/experiment3a/software/experiment3a/obj/default/experiment3a.d
@@ -0,0 +1,36 @@
+obj/default/experiment3a.o: experiment3a.c \
+  ../experiment3a_bsp//HAL/inc/sys/alt_alarm.h \
+  ../experiment3a_bsp//HAL/inc/sys/alt_llist.h \
+  ../experiment3a_bsp//HAL/inc/alt_types.h \
+  ../experiment3a_bsp//HAL/inc/priv/alt_alarm.h \
+  ../experiment3a_bsp//HAL/inc/alt_types.h \
+  ../experiment3a_bsp//HAL/inc/alt_types.h ../experiment3a_bsp/system.h \
+  ../experiment3a_bsp/linker.h \
+  ../experiment3a_bsp//drivers/inc/altera_avalon_performance_counter.h \
+  ../experiment3a_bsp//HAL/inc/alt_types.h \
+  ../experiment3a_bsp//HAL/inc/io.h \
+  ../experiment3a_bsp//HAL/inc/alt_types.h
+
+../experiment3a_bsp//HAL/inc/sys/alt_alarm.h:
+
+../experiment3a_bsp//HAL/inc/sys/alt_llist.h:
+
+../experiment3a_bsp//HAL/inc/alt_types.h:
+
+../experiment3a_bsp//HAL/inc/priv/alt_alarm.h:
+
+../experiment3a_bsp//HAL/inc/alt_types.h:
+
+../experiment3a_bsp//HAL/inc/alt_types.h:
+
+../experiment3a_bsp/system.h:
+
+../experiment3a_bsp/linker.h:
+
+../experiment3a_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../experiment3a_bsp//HAL/inc/alt_types.h:
+
+../experiment3a_bsp//HAL/inc/io.h:
+
+../experiment3a_bsp//HAL/inc/alt_types.h:
---
+++ Lab3/exercises/exercise3/experiment3c/software/experiment3c/obj/default/experiment3c.d
@@ -0,0 +1,36 @@
+obj/default/experiment3c.o: experiment3c.c \
+  ../experiment3c_bsp//HAL/inc/sys/alt_alarm.h \
+  ../experiment3c_bsp//HAL/inc/sys/alt_llist.h \
+  ../experiment3c_bsp//HAL/inc/alt_types.h \
+  ../experiment3c_bsp//HAL/inc/priv/alt_alarm.h \
+  ../experiment3c_bsp//HAL/inc/alt_types.h \
+  ../experiment3c_bsp//HAL/inc/alt_types.h ../experiment3c_bsp/system.h \
+  ../experiment3c_bsp/linker.h \
+  ../experiment3c_bsp//drivers/inc/altera_avalon_performance_counter.h \
+  ../experiment3c_bsp//HAL/inc/alt_types.h \
+  ../experiment3c_bsp//HAL/inc/io.h \
+  ../experiment3c_bsp//HAL/inc/alt_types.h
+
+../experiment3c_bsp//HAL/inc/sys/alt_alarm.h:
+
+../experiment3c_bsp//HAL/inc/sys/alt_llist.h:
+
+../experiment3c_bsp//HAL/inc/alt_types.h:
+
+../experiment3c_bsp//HAL/inc/priv/alt_alarm.h:
+
+../experiment3c_bsp//HAL/inc/alt_types.h:
+
+../experiment3c_bsp//HAL/inc/alt_types.h:
+
+../experiment3c_bsp/system.h:
+
+../experiment3c_bsp/linker.h:
+
+../experiment3c_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../experiment3c_bsp//HAL/inc/alt_types.h:
+
+../experiment3c_bsp//HAL/inc/io.h:
+
+../experiment3c_bsp//HAL/inc/alt_types.h:
